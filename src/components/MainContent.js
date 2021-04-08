08dec06d3f3d38658909e058df4b404d578944a3
Hasan Ocak <ocakhasan99@gmail.com>
2021-04-08 16:32:22 +0300

    fixed some css

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,40 +1,48 @@
 import React from "react";
 import "./styles/Products.css"
+import {Link} from 'react-router-dom'
 
 
-const MainContent = ({data}) => {
+const MainContent = ({ data }) => {
     console.log("in main content")
 
-    const listitems = data.map((item)=>
-    <div className="card" key={item._id}>
-        <div className="card_header">
-            <h2>
-               { item.name}
-            </h2>
-        </div>
-        <div className="card_image">
-            <img src={item.image} />
-        </div>
-        <div className="card_detail">
-           
-            <p>{item.description}</p>
-            <p className= "card_price">
-                {item.price}$
-            </p> 
-            <div className="card_button">Add to Cart</div>
-        </div>
-
-
-    </div> 
+    const listitems = data.map((item) =>
     
+
+        <Link to="/login" style={{ textDecoration: 'none' }}>
+            <div className="card" key={item._id}>
+                <div className="card_header">
+                    <h2>
+                        {item.name}
+                    </h2>
+                </div>
+                <div className="card_image">
+                    <img src={item.image} />
+                </div>
+                <div className="card_detail">
+
+                    <p className="card_price">
+                        {item.price}TL
+                    </p>
+                    <button className="card_button">Add to Cart</button>
+                </div>
+
+
+            </div>
+        </Link>
+
+
     );
     return (
-        <div className= "main_content">
-            <h1 className= "header">Products</h1>
-            {listitems}
+        <div>
+            <h1 className="header">Products</h1>
+            <div className="main_content">
+                {listitems}
+            </div>
+
         </div>
 
-        
+
     )
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import Navbar from "./Navbar.js"
-import Footer from "./Footer.js"
 import MainContent from "./MainContent.js"
 import "./styles/Products.css"
 
 const Products = ({data}) => {
     console.log("in products")
     return (
-        <div className="container">
-            <Navbar/>
+        <div className="product_container">
             <MainContent data= {data}/>     
-            <Footer/>
         </div>
     )
 }
