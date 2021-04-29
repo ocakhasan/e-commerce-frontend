7b13f82d75a9503c84f2e59e84a905e3924a2696
Burak Sekili <buraksekili@sabanciuniv.edu>
2021-04-29 03:17:40 +0300

    Add user profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,55 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Alert from '@material-ui/lab/Alert';
-import { Box } from '@material-ui/core';
-import { Link } from 'react-router-dom'
+import { Box } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Alert from "@material-ui/lab/Alert";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ProfileCard from "./ProfileCard";
 
-const Profile = () => {
+const Profile = ({ user, setUser }) => {
+  const [profileData, setProfileData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [userLocal, setUserLocal] = useState(false);
 
-    const [profileData, setProfileData] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [user, setUser] = useState(false)
-
-    useEffect(() => {
-        const logged = window.localStorage.getItem('logged')
-        if (logged) {
-            setProfileData(JSON.parse(logged))
-            setLoading(false)
-            setUser(true)
-        } else {
-            setUser(false)
-            setLoading(false)
-        }
-    }, [])
-
-    if (loading) {
-        return (
-            <div>
-                <Alert severity="infor">Loading Profile Page</Alert>
-                <CircularProgress />
-            </div>
-        )
-    } else if (!loading && !user) {
-        return (
-            <div>
-                <Alert severity="info">First you need to login to have a profile page. You can login from <Link to="/login">here</Link></Alert>
-            </div>
-        )
-    } else if (!loading && !profileData) {
-        return (
-            <Alert severity="error">There is a problem</Alert>
-        )
+  useEffect(() => {
+    const logged = window.localStorage.getItem("logged");
+    if (logged) {
+      setProfileData(JSON.parse(logged));
+      setLoading(false);
+      setUserLocal(true);
     } else {
-        return (
-            <Box>
-                <Box>
-                    <h1>{profileData.username}</h1>
-                </Box>
-            </Box>
-        )
-
+      setUserLocal(false);
+      setLoading(false);
     }
+  }, []);
 
-}
+  if (loading) {
+    return (
+      <div>
+        <Alert severity="infor">Loading Profile Page</Alert>
+        <CircularProgress />
+      </div>
+    );
+  } else if (!loading && !userLocal) {
+    return (
+      <div>
+        <Alert severity="info">
+          First you need to login to have a profile page. You can login from{" "}
+          <Link to="/login">here</Link>
+        </Alert>
+      </div>
+    );
+  } else if (!loading && !profileData) {
+    return <Alert severity="error">There is a problem</Alert>;
+  } else {
+    return (
+      <Box>
+        <ProfileCard profile={profileData} user={user} setUser={setUser} />
+      </Box>
+    );
+  }
+};
 
-export default Profile
+export default Profile;
