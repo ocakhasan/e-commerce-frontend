2417b215f46da4c46af8fa4ccb8d001ef3a1f7dd
Burak Sekili <buraksekili@sabanciuniv.edu>
2021-05-06 13:50:12 +0300

    Add carousel for home page

--- src/App.js
+++ src/App.js
@@ -1,110 +1,122 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
-import Cart from './components/Cart';
-import Dashboard from './components/Dashboard';
-import Home from './components/Home';
-import LoginForm from './components/LoginForm';
-import Navbar from './components/Navbar';
-import ProductDetail from './components/ProductDetail';
-import Products from './components/Products';
-import Profile from './components/Profile';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Cart from "./components/Cart";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import LoginForm from "./components/LoginForm";
+import MainContent from "./components/MainContent";
+import Navbar from "./components/Navbar";
+import ProductDetail from "./components/ProductDetail";
+import Profile from "./components/Profile";
 import SignUpForm from "./components/SignupForm";
-import UpdateForm from './components/Update';
-import cartService from './services/cartService';
-import commentService from './services/commentService';
-import loginService from './services/loginService';
-import productService from './services/productService';
-
+import UpdateForm from "./components/Update";
+import cartService from "./services/cartService";
+import commentService from "./services/commentService";
+import loginService from "./services/loginService";
+import productService from "./services/productService";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [data, setData] = useState(null);
+  const [searchResults, setSearchResults] = useState(undefined);
+  const d = { data, setData, searchResults, setSearchResults };
 
-    const [user, setUser] = useState(null)
-    const history = useHistory()
-    
-    useEffect(() => {
-        const loggedUserJSON= window.localStorage.getItem('logged')
-        if (loggedUserJSON){
-            const userLocal = JSON.parse(loggedUserJSON)
-            console.log("user", userLocal)
-            console.log("token", userLocal.token)
-            productService.setToken(userLocal.token)
-            commentService.setToken(userLocal.token)
-            cartService.setToken(userLocal.token)
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("logged");
+    if (loggedUserJSON) {
+      const userLocal = JSON.parse(loggedUserJSON);
+      console.log("user", userLocal);
+      console.log("token", userLocal.token);
+      productService.setToken(userLocal.token);
+      commentService.setToken(userLocal.token);
+      cartService.setToken(userLocal.token);
 
-            setUser(userLocal)
-        }
-    }, [])
-
-    const handleLogin = async (userObject) => {
-        try{
-            console.log(userObject)
-            const response = await loginService.login(userObject)
-            window.localStorage.setItem(
-                'logged', JSON.stringify(response.user)
-            )
-            setUser(response.user)
-            productService.setToken(response.user.token)
-            commentService.setToken(response.user.token)
-            cartService.setToken(response.user.token)
-            return response.status
-            
-        } catch(exception) {
-            console.log("ex", exception) 
-        }
+      setUser(userLocal);
     }
+  }, []);
 
-    const handleLogout = () => {
-        window.localStorage.removeItem('logged')
-        setUser(null)
+  const handleLogin = async (userObject) => {
+    try {
+      console.log(userObject);
+      const response = await loginService.login(userObject);
+      window.localStorage.setItem("logged", JSON.stringify(response.user));
+      setUser(response.user);
+      productService.setToken(response.user.token);
+      commentService.setToken(response.user.token);
+      cartService.setToken(response.user.token);
+      return response.status;
+    } catch (exception) {
+      console.log("ex", exception);
     }
+  };
 
+  const handleLogout = () => {
+    window.localStorage.removeItem("logged");
+    setUser(null);
+  };
 
+  return (
+    <Router>
+      <div className="container">
+        <Navbar
+          user={user}
+          handleLogout={handleLogout}
+          data={data}
+          setData={setData}
+          searchResults={searchResults}
+          setSearchResults={setSearchResults}
+        />
+        <Switch>
+          <Route exact path="/login">
+            <LoginForm handleLogin={handleLogin} />
+          </Route>
 
-    return (
-        <Router>
-            <div className="container">
-                <Navbar user={user} handleLogout={handleLogout}/>
-                <Switch>
-                    <Route exact path="/login">
-                        < LoginForm handleLogin={handleLogin}/>
-                    </Route>
+          <Route exact path="/signup">
+            <SignUpForm />
+          </Route>
 
-                    <Route exact path="/signup">
-                        <SignUpForm />
-                    </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
 
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
 
-                    <Route exact path="/dashboard">
-                        <Dashboard />
-                    </Route>
+          <Route exact path="/product/:id">
+            <ProductDetail />
+          </Route>
 
-                    <Route exact path="/cart">
-                        <Cart />
-                    </Route>
+          <Route exact path="/update/product/:id">
+            <UpdateForm />
+          </Route>
 
-                    <Route exact path="/product/:id">
-                        <ProductDetail />
-                    </Route>
+          <Route exact path="/products">
+            <MainContent
+              data={data}
+              setData={setData}
+              searchResults={searchResults}
+              setSearchResults={setSearchResults}
+            />
+          </Route>
 
-                    <Route exact path="/update/product/:id">
-                        <UpdateForm />
-                    </Route>
+          <Route exact path="/profile">
+            <Profile setUser={setUser} user={user} />
+          </Route>
 
-                    <Route exact path="/products">
-                        <Products />
-                    </Route>
-
-                    <Route exact path="/profile">
-                        <Profile setUser={setUser} user={user} />
-                    </Route>
-
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+          <Route exact path="/">
+            <Home data={data} />
+            <MainContent
+              data={data}
+              setData={setData}
+              searchResults={searchResults}
+              setSearchResults={setSearchResults}
+            />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,90 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import ExploreIcon from "@material-ui/icons/Explore";
 import React from "react";
+import Carousel from "react-material-ui-carousel";
 import { useHistory } from "react-router-dom";
-import Products from "./Products"
-import './styles/home.css'
+import "./styles/home.css";
+import "./styles/Products.css";
 
-const Home = () => {
+const Home = ({ data }) => {
+  const history = useHistory();
+  const len = data ? (data.length % 3) + 1 : 0;
+  return (
+    <>
+      <div className="home">
+        <div className="home-header">
+          <Typography variant="h3" component="h3">
+            Shop from the Best
+          </Typography>
+          <Typography variant="h5" component="h3">
+            Explore hundreds of products within seconds with perfect user
+            experience
+          </Typography>
 
-    const history = useHistory()
 
-    return (
-        <div>
-            <div className="home">
-                <div className="home-header">
-                    <h1>Shop from the Best</h1>
-                    <h2>Explore hundreds of products within seconds with perfect user experience</h2>
-                    <button className="home-button" onClick={() => history.push("/products")}>Explore</button>
-                </div>
-                <img src={process.env.PUBLIC_URL + '/basketball.jpg'} alt="product" className="home-img"></img>
-            </div>
-            <Products />
+          {len > 0 && (
+            <Carousel 
+            timeout={250} 
+            navButtonsAlwaysInvisible
+            animation="slide"
+            className="product_container">
+              {data?.slice(0, 3).map((product, i) => (
+                <Card style={{marginLeft: 15, marginRight: 15}}>
+                  <CardActionArea>
+                    <CardMedia
+                      style={{objectFit: "contain"}}
+                      component="img"
+                      height="200"
+                      src={product.imagePath}
+                      image={product.imagePath}
+                      title={product.productName}
+                    />
+                  </CardActionArea>
+                  <CardContent>
+                    <Typography
+                      style={{ textAlign: "center", marginTop: 14 }}
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                    >
+                      {product.productName}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              ))}
+            </Carousel>
+          )}
+          
+          {/* <Button
+            size="large"
+            style={{ marginTop: 15 }}
+            variant="outlined"
+            onClick={() => history.push("/products")}
+            endIcon={<ExploreIcon style={{ padding: 0 }} />}
+          >
+            Explore
+          </Button> */}
         </div>
-    )
-}
+        <img
+          src={process.env.PUBLIC_URL + "/basketball.jpg"}
+          alt="product"
+          className="home-img"
+        ></img>
+      </div>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Divider style={{ width: "50%", margin: 10, marginTop: 20 }} />
+      </Grid>
+    </>
+  );
+};
 
-export default Home
+export default Home;
